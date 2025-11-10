59e6408bf542b3334f907c92d0c2c124fbda36ba
Tomas Krajcovic <krajcovic.t@gmail.com>
2025-11-10 16:56:59 +0100

    store not defined - error

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-full rounded-lg bg-gray-200">
     <Sidebar :class="{ '-ml-[205px]' : !sidebarOpened}" />
     <div class="flex-1">
-        <Navbarr @toggle-sidebar="toggleSidebar"/>
+        <Navbar @toggle-sidebar="toggleSidebar"/>
         <main class="p-5">
             <div class="p-4 rounded bg-white">
               <router-view></router-view>
@@ -15,7 +15,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import Sidebar from './Sidebar.vue';
-import Navbarr from './Navbar.vue';
+import Navbar from './Navbar.vue';
 
 const {title} = defineProps({
     title: String
@@ -27,7 +27,9 @@ function toggleSidebar() {
     sidebarOpened.value = !sidebarOpened.value;
 }
 onMounted(() => {
-    /* store.dispatch('getUser'); */
+    /* Uncaught (in promise) ReferenceError: store is not defined */
+    store.dispatch('getUser');
+    /*  */
     handleResize();
     window.addEventListener('resize', handleResize);
 });
