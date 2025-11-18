adfc9ce547631de81370b9c958a112c535250ced
Tomas Krajcovic <krajcovic.t@gmail.com>
2025-11-18 03:16:39 +0100

    Bug - not generating slug

--- database/migrations/2025_10_20_194742_create_products_table.php
+++ database/migrations/2025_10_20_194742_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title', 2000);
-            $table->string('slug', 2000);
+            $table->string('slug', 2000)->nullable();
             $table->string('image', 2000)->nullable();
             $table->string('image_mime')->nullable();
             $table->integer('image_size')->nullable();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            AdminUserSeeder::class,
+            ProductSeeder::class,
+        ]);
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
     }
 }
