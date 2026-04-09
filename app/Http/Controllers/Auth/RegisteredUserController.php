995ce3d69e57efc74097ecdb4aefb7fb73e8a11c
Tomas Krajcovic <krajcovic.t@gmail.com>
2026-04-09 17:58:25 +0200

    Insert Customer in DB on registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Helpers\Cart;
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -44,6 +45,13 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
+
+        $customer = new Customer();
+        $names = explode(" ", $user->name);
+        $customer->user_id = $user->id;
+        $customer->first_name = $names[0];
+        $customer->last_name = $names[1] ?? '';
+        $customer->save();
 
         Auth::login($user);
 
