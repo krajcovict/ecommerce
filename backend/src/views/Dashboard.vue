5d2e3a6deed65bab173837a052cf576fc14a9a05
Tomas Krajcovic <krajcovic.t@gmail.com>
2026-06-09 11:57:45 +0200

    Create Dashboard cards

--- backend/src/views/Dashboard.vue
+++ backend/src/views/Dashboard.vue
@@ -1,7 +1,22 @@
 <template>
-    <h1>Dashboard</h1>
-    <div class="login">
-
+    <h1 class="text-3xl font-semibold">Dashboard</h1>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-3 py-3">
+        <div class="bg-white p-5 rounded-lg border border-gray-400 shadow-lg flex flex-col items-center justify-center">
+            <label>Active Customers</label>
+            <span class="text-3xl font-semibold">15</span>
+        </div>
+        <div class="bg-white p-5 rounded-lg border border-gray-400 shadow-lg flex flex-col items-center justify-center">
+            <label>Active Products</label>
+            <span class="text-3xl font-semibold">15</span>
+        </div>
+        <div class="bg-white p-5 rounded-lg border border-gray-400 shadow-lg flex flex-col items-center justify-center">
+            <label>Paid Orders</label>
+            <span class="text-3xl font-semibold">15</span>
+        </div>
+        <div class="bg-white p-5 rounded-lg border border-gray-400 shadow-lg flex flex-col items-center">
+            <label>Total Income</label>
+            <span class="text-2xl font-semibold">$28,003,562</span>
+        </div>
     </div>
 </template>
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
@@ -23,6 +24,12 @@ Route::middleware(['auth:sanctum', 'admin'])
         Route::get('orders/statuses', [OrderController::class, 'getStatuses']);
         Route::post('orders/change-status/{order}/{status}', [OrderController::class, 'changeStatus']);
         Route::get('orders/{order}', [OrderController::class, 'view']);
+
+        // Dashboard routes:
+        Route::get('/dashboard/customers-count', [DashboardController::class, 'activeCustomers']);
+        Route::get('/dashboard/products-count', [DashboardController::class, 'activeProducts']);
+        Route::get('/dashboard/orders-count', [DashboardController::class, 'paidOrders']);
+        Route::get('/dashboard/income-amount', [DashboardController::class, 'totalIncome']);
     });
 
 Route::post('/login', [AuthController::class, 'login']);
