265f084e9f45d981f83a551fa8490d589e77a5de
Tomas Krajcovic <krajcovic.t@gmail.com>
2026-07-23 13:16:58 +0200

    Output multiple images on the product view page

--- resources/views/product/view.blade.php
+++ resources/views/product/view.blade.php
@@ -13,24 +13,25 @@
           <div class="lg:col-span-3">
             <div
               x-data="{
-  images: ['{{ $product->image ?: '/img/noimage.png' }}', 'img/1_2.jpg', 'img/1_3.jpg', 'img/1_4.jpg'],
-  activeImage: null,
-  prev() {
-      let index = this.images.indexOf(this.activeImage);
-      if (index === 0)
-          index = this.images.length;
-      this.activeImage = this.images[index - 1];
-  },
-  next() {
-      let index = this.images.indexOf(this.activeImage);
-      if (index === this.images.length - 1)
-          index = -1;
-      this.activeImage = this.images[index + 1];
-  },
-  init() {
-      this.activeImage = this.images.length > 0 ? this.images[0] : null
-  }
-}"
+                images: {{ $product->images->map(fn($im) => $im->url) }},
+                //images: ['{{ $product->image ?: '/img/noimage.png' }}', 'img/1_2.jpg', 'img/1_3.jpg', 'img/1_4.jpg'],
+                activeImage: null,
+                prev() {
+                    let index = this.images.indexOf(this.activeImage);
+                    if (index === 0)
+                        index = this.images.length;
+                    this.activeImage = this.images[index - 1];
+                },
+                next() {
+                    let index = this.images.indexOf(this.activeImage);
+                    if (index === this.images.length - 1)
+                        index = -1;
+                    this.activeImage = this.images[index + 1];
+                },
+                init() {
+                    this.activeImage = this.images.length > 0 ? this.images[0] : null
+                }
+              }"
             >
               <div class="relative">
                 <template x-for="image in images">
